b5769fab740bab6d01e5971ac70dbd12fcfd0d68
Stephan van Eijkelenburg <stephanvane@gmail.com>
2011-01-18 00:37:22 +0100

    added colorbox / did some other things

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -1,12 +1,9 @@
 class BoatsController < ApplicationController
   def index
     @boats = Boat.all
-    render :layout => '2columns'
   end
   
   def show
     @boat = Boat.find_first_by_url(params[:url])
-    puts "#{@boat.inspect} <--"
-    render :layout => '2columns'
   end
 end
